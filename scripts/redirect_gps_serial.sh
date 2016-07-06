7ddb6ddfc57feed81fddb82928127c7171094322
Ryan David <ryandavid773@gmail.com>
2016-07-05 22:58:37 -0700

    Update scripts for target device

--- scripts/redirect_gps_serial.sh
+++ scripts/redirect_gps_serial.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# This runs on the rotobox and forwards the GPS over TCP port 54321
-socat tcp-l:54321,reuseaddr,fork file:/dev/ttyS1,nonblock,raw,echo=0
-
-# Then on Windows, open u-center and 'Receiver' -> 'Port' -> 'Network Connection'
+# This runs on the rotobox and forwards the GPS over TCP port 54321.
+# Jump over to a Windows machine, open u-center: 'Receiver' -> 'Port' -> 'Network Connection'
 # Use 'tcp://<ip_of_rotobox>:54321'
+
+ips=$(ifconfig | awk '/inet addr/{print substr($2,6)}')
+
+echo "Redirecting ttyS1 (GPS):"
+for ip in $ips
+do
+	echo "  tcp://$ip:54321"
+done
+echo ""
+echo "Press Ctrl-C to stop..."
+
+socat tcp-l:54321,reuseaddr,fork file:/dev/ttyS1,nonblock,raw,echo=0

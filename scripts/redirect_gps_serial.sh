89ecdeea75a50805b1a7b78a5f0b4a91c5c6a885
Ryan David <ryandavid773@gmail.com>
2016-07-05 20:50:09 -0700

    Add fork of gpsd

--- scripts/redirect_gps_serial.sh
+++ scripts/redirect_gps_serial.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-socat tcp-l:54321,reuseaddr,fork file:/dev/ttyS1,nonblock
+# This runs on the rotobox and forwards the GPS over TCP port 54321
+socat tcp-l:54321,reuseaddr,fork file:/dev/ttyS1,nonblock,raw,echo=0
+
+# Then on Windows, open u-center and 'Receiver' -> 'Port' -> 'Network Connection'
+# Use 'tcp://<ip_of_rotobox>:54321'
